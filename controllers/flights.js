e5999d4692a5b4c0a116ad592d387318c883c0e3
korycfitz <korycfitz@gmail.com>
2023-04-18 20:12:57 -0400

    add edit route, stub up controller function, and add functionality to it

--- controllers/flights.js
+++ controllers/flights.js
@@ -58,10 +58,26 @@ function deleteFlight(req, res) {
   })
 }
 
+function edit(req, res) {
+  Flight.findById(req.params.movieId)
+  .then(flight => {
+    res.render("flights/edit", {
+      flight: flight,
+      title: "Edit Flight"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+
+
 export{
   newFlight as new,
   create,
   index,
   show,
   deleteFlight as delete,
+  edit,
 }
