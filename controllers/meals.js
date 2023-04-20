11c97c8efb4c6814fd1562d2625ce4bec5e18530
korycfitz <korycfitz@gmail.com>
2023-04-19 23:50:35 -0400

    add meals/new.ejs view containing a form for entering new meals

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,7 +1,17 @@
 import { Meal } from '../models/meal.js'
 
 function newMeal(req, res) {
-  
+  Meal.find({})
+  .then(meals => {
+    res.render('meals/new', {
+      title: 'Add Meal',
+      meals: meals,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/flights")
+  })
 }
 
 export {
